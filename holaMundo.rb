00350d9636c755b2bc7d5226db687ef1390b72a3
Missael Armenta <missael306@gmail.com>
2018-08-13 17:16:57 -0500

    Parsing values  int to string and viceverza

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,6 +1,9 @@
 =begin
-    Implementando variables:
-    A pesar de ser un lenguaje tipado no es necesario indicar el tipo de variable a emplear 
+    Parseando valores 
+    If you want parse the value of a variable you must use to_'TYPE-OF-VALUE'
+    Example:
+        nombre = "20"
+        numero = nombre.to_i
 =end
 class HolaMundo
     
@@ -14,9 +17,18 @@ class HolaMundo
         resultado = num1 + num2 
         puts resultado 
     end
+    def parseInt(numeroString)
+        resultado = numeroString.to_i + 5
+        puts "Se parseo el número #{numeroString}"
+        puts "Si al número parseado se le suman 5 el resultado es: #{resultado}"
+    end
+    def parseString(numero)
+        cadena = numero.to_s
+        puts "Ahora el numero #{cadena} se comporta como cadena"
+    end
 end
 
 object = HolaMundo.new()
-object.suma(5,5)
-object.saluda("Missael")
+object.parseInt("30")
+object.parseString(50)
 gets() #this stop the flow 
