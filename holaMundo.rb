d9ffecbea1bd4a01b897c126ccd97590aba66ebe
Missael Armenta <missael306@gmail.com>
2018-08-13 17:34:09 -0500

    Using diferents scopes

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,13 +1,17 @@
 =begin
-    Parseando valores 
-    If you want parse the value of a variable you must use to_'TYPE-OF-VALUE'
-    Example:
-        nombre = "20"
-        numero = nombre.to_i
+    Variable's scope
+    In ruby the scope of the variables is defindeb by 
+    $variableName - Global variable
+    @variableName - Instance variable
+    varianleName - Local variable
 =end
-class HolaMundo
-    
-    def init()
+
+$saludoGlobal = "Hola desde el scope global"
+
+class Laboratory
+    def initialize()
+        #this is the construct of the class 
+        @saludoInstancia = "Hola desde el scope de la clase"
     end
     def saluda(nombre)
         puts "Buend día #{nombre}" # The symbol #{} is used to evalueted an expression 
@@ -18,17 +22,24 @@ class HolaMundo
         puts resultado 
     end
     def parseInt(numeroString)
+        #parging strint to integer
         resultado = numeroString.to_i + 5
         puts "Se parseo el número #{numeroString}"
         puts "Si al número parseado se le suman 5 el resultado es: #{resultado}"
     end
     def parseString(numero)
+        #parsing integer to String
         cadena = numero.to_s
         puts "Ahora el numero #{cadena} se comporta como cadena"
     end
+    def scopeVariable()
+        saludaLocal = "Hola desde el metodo scopeVariable"
+        puts $saludoGlobal
+        puts @saludoInstancia
+        puts saludaLocal
+    end
 end
 
-object = HolaMundo.new()
-object.parseInt("30")
-object.parseString(50)
+object = Laboratory.new()
+object.scopeVariable()
 gets() #this stop the flow 
